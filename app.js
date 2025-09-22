b428a4f0ddfbf631724abd42c5b0e1b0b6736e04
mike@mikebarb.net <mike@mikebarb.net>
2025-09-22 21:51:05 +1000

    added scanner formats.

--- app.js
+++ app.js
@@ -114,7 +114,14 @@ function initializeApp() {
             { 
                 fps: 10,
                 qrbox: 250,
-                rememberLastUsedCamera: true
+                rememberLastUsedCamera: true,
+                supportedFormats: [ 
+                    Html5QrcodeSupportedFormats.QR_CODE,
+                    Html5QrcodeSupportedFormats.CODE_128,
+                    Html5QrcodeSupportedFormats.CODE_39,
+                    Html5QrcodeSupportedFormats.EAN_13,
+                    Html5QrcodeSupportedFormats.UPC_A
+                ]
             },
             false
         );
